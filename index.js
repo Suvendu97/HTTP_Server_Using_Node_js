3fed032812b0bf2e0e3a5520033acda000972f3b
Suvendu97 <57838911+Suvendu97@users.noreply.github.com>
2020-07-19 22:17:58 +0530

    initial commiy

---
+++ index.js
@@ -0,0 +1,41 @@
+const http=require('http');
+const port=8000;
+const fs=require('fs');
+function requestHaldler(req, res) {
+    console.log(req.url);
+    res.writeHead(200, {'content-type': 'text/html'});
+    
+    let filePath;
+    switch(req.url) {
+        case '/':
+            filePath='./index.html'
+            break;
+        case '/profile':
+            filePath='./profile.html'
+            break;
+        default:
+            filePath='./404.html'
+
+    }
+
+    fs.readFile(filePath, function(err,data) {
+        if(err) {
+            console.log('err');
+            return res.end('<h1>error</h1>');
+        }
+
+        return res.end(data);
+    })
+    
+}
+
+
+const server=http.createServer(requestHaldler);
+
+server.listen(port,function(err) {
+    if(err) {
+        console.log(err);
+        return;
+    }
+    console.log('server is up and running on port ', port);
+});
